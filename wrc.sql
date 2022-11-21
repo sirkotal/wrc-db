59d3e77d81dc3ea83612e09f25cd43f26749b836
sirkotal <lordkotal2002@gmail.com>
2022-11-21 15:24:48 +0000

    Testing Rally class implementation in SQL

--- wrc.sql
+++ wrc.sql
@@ -1,5 +1,20 @@
 PRAGMA foreign_keys = on;
 BEGIN TRANSACTION;
 
+-- Table: Rally
+DROP TABLE IF EXISTS Rally;
+
+CREATE TABLE Rally (
+    idRally      INTEGER PRIMARY KEY,
+    startDate    VARCHAR(100) CONSTRAINT unique_startDate UNIQUE CONSTRAINT startDate_nn NOT NULL,
+    endDate      VARCHAR(100) CONSTRAINT unique_endDate UNIQUE CONSTRAINT endDate_nn NOT NULL,
+    rallyName    VARCHAR(1000),
+    hq           VARCHAR(1000) CONSTRAINT unique_hq UNIQUE CONSTRAINT hq_nn NOT NULL,
+    surface      VARCHAR(100) CONSTRAINT surface_nn NOT NULL,
+    stages       INTEGER CONSTRAINT stages_nn NOT NULL,
+    distance     FLOAT CONSTRAINT distance_nn NOT NULL,
+    PRIMARY KEY(idRally, rallyName)
+);
+
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
