f7550ddce788ec219436f61425b788898942c130
sirkotal <lordkotal2002@gmail.com>
2022-11-21 18:32:37 +0000

    Fixed a bug in Rally and added event data

---
+++ data.sql
@@ -0,0 +1,13 @@
+INSERT INTO Rally VALUES(1, '2022-01-20', '2022-01-23', 'Rallye Automobile Monte Carlo', 'Monte Carlo, Monaco', 'Mixed', 17, 296.03);
+INSERT INTO Rally VALUES(2, '2022-02-24', '2022-02-27', 'Rally Sweden', 'Umeå, Västerbotten County', 'Snow', 17, 264.81);
+INSERT INTO Rally VALUES(3, '2022-04-21', '2022-04-24', 'Croatia Rally', 'Zagreb', 'Tarmac', 20, 291.84);
+INSERT INTO Rally VALUES(4, '2022-05-19', '2022-05-22', 'Rally de Portugal', 'Matosinhos, Porto', 'Gravel', 21, 296.03);
+INSERT INTO Rally VALUES(5, '2022-06-02', '2022-06-05', 'Rally Italia Sardegna', 'Alghero, Sardinia', 'Gravel', 21, 307.91);
+INSERT INTO Rally VALUES(6, '2022-06-23', '2022-06-26', 'Safari Rally Kenya', 'Nairobi', 'Gravel', 19, 363.44);
+INSERT INTO Rally VALUES(7, '2022-07-14', '2022-07-17', 'Rally Estonia', 'Tartu, Tartu County', 'Gravel', 24, 314.26);
+INSERT INTO Rally VALUES(8, '2022-08-04', '2022-08-07', 'Rally Finland', 'Jyväskylä, Central Finland', 'Gravel', 22, 322.61);
+INSERT INTO Rally VALUES(9, '2022-08-18', '2022-08-21', 'Ypres Rally Belgium', 'Ypres, West Flanders', 'Tarmac', 20, 281.58);
+INSERT INTO Rally VALUES(10, '2022-09-08', '2022-09-11', 'Acropolis Rally Greece', 'Lamia, Central Greece', 'Gravel', 16, 303.30);
+INSERT INTO Rally VALUES(11, '2022-09-29', '2022-10-02', 'Rally New Zealand', 'Auckland, North Island', 'Gravel', 17, 279.80);
+INSERT INTO Rally VALUES(12, '2022-10-20', '2022-10-23', 'RACC Rally Catalunya de España', 'Salou, Catalonia', 'Tarmac', 19, 293.77);
+INSERT INTO Rally VALUES(13, '2022-11-10', '2022-11-13', 'Rally Japan', 'Nagoya, Chūbu region', 'Tarmac', 19, 283.27);
--- wrc.sql
+++ wrc.sql
@@ -5,15 +5,16 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS Rally;
 
 CREATE TABLE Rally (
-    idRally      INTEGER PRIMARY KEY,
-    startDate    VARCHAR(100) CONSTRAINT unique_startDate UNIQUE CONSTRAINT startDate_nn NOT NULL,
-    endDate      VARCHAR(100) CONSTRAINT unique_endDate UNIQUE CONSTRAINT endDate_nn NOT NULL,
+    idRally      INTEGER,
+    startDate    DATE CONSTRAINT unique_startDate UNIQUE CONSTRAINT startDate_nn NOT NULL,
+    endDate      DATE CONSTRAINT unique_endDate UNIQUE CONSTRAINT endDate_nn NOT NULL,
     rallyName    VARCHAR(1000),
     hq           VARCHAR(1000) CONSTRAINT unique_hq UNIQUE CONSTRAINT hq_nn NOT NULL,
     surface      VARCHAR(100) CONSTRAINT surface_nn NOT NULL,
     stages       INTEGER CONSTRAINT stages_nn NOT NULL,
     distance     FLOAT CONSTRAINT distance_nn NOT NULL,
-    PRIMARY KEY(idRally, rallyName)
+    PRIMARY KEY(idRally, rallyName),
+    CONSTRAINT date_match CHECK ( (startDate <= endDate))
 );
 
 COMMIT TRANSACTION;
